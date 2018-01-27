a684ea90c5ab0f0f034d09fb11ed8793a346253c
heeinso <heeinso@snu.ac.kr>
2018-01-27 17:13:00 +0900

    Modify directory structure

---
+++ app/src/main/java/kr/ac/kaist/kotlinpractice/util/Util.kt
@@ -0,0 +1,11 @@
+package kr.ac.kaist.kotlinpractice.util
+
+import android.support.annotation.IdRes
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by stevehan on 2018. 1. 27..
+ */
+fun AppCompatActivity.replace(@IdRes frameId: Int, fragment: android.support.v4.app.Fragment, tag: String? = null) {
+    supportFragmentManager.beginTransaction().replace(frameId, fragment, tag).commit()
+}
